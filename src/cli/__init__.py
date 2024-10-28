55e49cebbf0ba5260421cf5ed54198897f6c6a76
Undead34 <maizogabriel@gmail.com>
2024-10-28 09:39:33 -0400

    update

--- src/cli/__init__.py
+++ src/cli/__init__.py
@@ -5,8 +5,6 @@ def parse_arguments() -> argparse.Namespace:
     parser.add_argument('-i', '--interactive', action='store_true', help='modo interactivo')
     parser.add_argument('--config-file', type=str, help='ruta al archivo de configuración')
     parser.add_argument('-d', '--debug', action='store_true', help='activar modo debug')
-    parser.add_argument('-v', '--verbose', action='store_true', help='activar salida detallada')
-    parser.add_argument('-e', '--export', action='store_true', help='activar salida csv')
     
     if not len(sys.argv) > 1:
         parser.print_help()
